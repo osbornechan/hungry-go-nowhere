578e36b3fbb9575c60ecdf605cadf9728b73a93e
osbornechan <osbornecab@gmail.com>
2020-05-05 00:42:43 +0800

    Connect to database

--- index.js
+++ index.js
@@ -34,6 +34,23 @@ app.engine('jsx', reactEngine);
 // db contains *ALL* of our models
 const allModels = require('./db');
 
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
+
 // ===================================
 //              Routes
 // ===================================
