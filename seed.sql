260a3a120d5d526a5aa0e968c122435fd02f4b9b
osbornechan <osbornecab@gmail.com>
2020-05-04 21:57:12 +0800

    Minor change to seed.sql

--- seed.sql
+++ seed.sql
@@ -21,11 +21,11 @@ INSERT INTO products (product_name, brand, img, category_id) VALUES ('Carrots','
 INSERT INTO products (product_name, brand, img, category_id) VALUES ('Egg Tofu','Unicurd','https://s3-ap-southeast-1.amazonaws.com/www8.fairprice.com.sg/fpol/media/images/product/XL/13012876_LXL1.jpg',1);
 INSERT INTO products (product_name, brand, img, category_id) VALUES ('Pizza','Ristorante','https://www.waangoo.com/content/images/thumbs/0019546_dr-oetker-ristorante-mozzarella-pizza-frozen_600.jpeg',5);
 INSERT INTO products (product_name, brand, img, category_id) VALUES ('Vanilla Ice Cream','Movenpick','https://s3-ap-southeast-1.amazonaws.com/www8.fairprice.com.sg/fpol/media/images/product/XL/13002251_LXL1.jpg',5);
-INSERT INTO products (product_name, brand, img, category_id) VALUES ('Orange Juice',"Florida's Natural",'https://s3-ap-southeast-1.amazonaws.com/www8.fairprice.com.sg/fpol/media/images/product/XL/12570035_LXL1.jpg',7);
+INSERT INTO products (product_name, brand, img, category_id) VALUES ('Orange Juice','Floridas Natural','https://s3-ap-southeast-1.amazonaws.com/www8.fairprice.com.sg/fpol/media/images/product/XL/12570035_LXL1.jpg',7);
 INSERT INTO products (product_name, brand, img, category_id) VALUES ('Cream Crackers','Hup Seng','https://s3-ap-southeast-1.amazonaws.com/www8.fairprice.com.sg/fpol/media/images/product/XL/12374819_LXL1.jpg',6);
-INSERT INTO products (product_name, brand, img, category_id) VALUES ('Cream of Mushroom',"Campbell's",'https://s3-ap-southeast-1.amazonaws.com/www8.fairprice.com.sg/fpol/media/images/product/XL/274663_LXL1.jpg',4);
+INSERT INTO products (product_name, brand, img, category_id) VALUES ('Cream of Mushroom','Campbells','https://s3-ap-southeast-1.amazonaws.com/www8.fairprice.com.sg/fpol/media/images/product/XL/274663_LXL1.jpg',4);
 INSERT INTO products (product_name, brand, img, category_id) VALUES ('Coconut Water','UFC','https://thesmartlocal.com/reviews/wp-content/uploads/2016/01/ufc-refresh-coconut-water-62-1453194498.jpg',7);
-INSERT INTO products (product_name, brand, img, category_id) VALUES ('Chocolate Peanuts',"M&M's",'https://s3-ap-southeast-1.amazonaws.com/www8.fairprice.com.sg/fpol/media/images/product/XL/12683734_LXL1.jpg',6);
+INSERT INTO products (product_name, brand, img, category_id) VALUES ('Chocolate Peanuts','M&Ms','https://s3-ap-southeast-1.amazonaws.com/www8.fairprice.com.sg/fpol/media/images/product/XL/12683734_LXL1.jpg',6);
 
 
 /* SEED DATA FOR CATEGORIES */
