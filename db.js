0bd1e83080d968f707c641909bd1a0acc92431b6
osbornechan <osbornecab@gmail.com>
2020-05-05 09:26:21 +0800

    Added postgres url

--- db.js
+++ db.js
@@ -7,6 +7,8 @@ const url = require('url');
 
 var configs;
 
+process.env.DATABASE_URL="postgres://dlqbxgkddsemvc:6abcd8f01d2550023a55477dec8648af21a65df7d9907b973722162a774e40d9@ec2-34-200-72-77.compute-1.amazonaws.com:5432/d5aa1ovjcad7tb"
+
 if (process.env.DATABASE_URL) {
     const params = url.parse(process.env.DATABASE_URL);
     const auth = params.auth.split(':');
@@ -29,8 +31,6 @@ if (process.env.DATABASE_URL) {
 }
 
 const pool = new pg.Pool(configs);
-//console.log(configs)
-console.log(pool)
 
 pool.on('error', function (err) {
     console.log('idle client error', err.message, err.stack);
