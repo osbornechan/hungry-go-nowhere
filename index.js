a592ca879d3f1d91089a2324815940b2ad87d791
osbornechan <osbornecab@gmail.com>
2020-05-05 09:37:23 +0800

    Revert

--- index.js
+++ index.js
@@ -3,23 +3,6 @@ const methodOverride = require('method-override');
 const cookieParser = require('cookie-parser');
 const sha256 = require('js-sha256');
 
-const { Client } = require('pg');
-
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
-});
-
-client.connect();
-
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
-});
-
   // ===================================
   //     Configurations and set up
   // ===================================
